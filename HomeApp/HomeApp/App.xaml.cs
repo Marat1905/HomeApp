e710f96aaec7d5579ac021032bccef2ba80f7fe9
Marat <milya010790@mail.ru>
2024-08-13 19:56:01 +0500

    Добавьте файлы проекта.

---
+++ HomeApp/HomeApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HomeApp.Pages;
+
+namespace HomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            // инициализация интерфейса
+            InitializeComponent();
+            // Инициализация главного экрана
+            MainPage = new RegisterPage(); // new LoadingPage();  new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
