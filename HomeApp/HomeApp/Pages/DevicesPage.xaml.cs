8a10784ed778bb5a09eed9e8ca61b80d8ed1a549
Marat <milya010790@mail.ru>
2024-08-14 19:03:28 +0500

    Добавили Frame

--- HomeApp/HomeApp/Pages/DevicesPage.xaml.cs
+++ HomeApp/HomeApp/Pages/DevicesPage.xaml.cs
@@ -20,38 +20,49 @@ namespace HomeApp.Pages
             // Создадим некий список устройств.
             // В реальном приложении они могут доставаться из базы или веб-сервиса.
             var homeDevices = new List<string>()
-           {
-               "Чайник",
-               "Стиральная машина",
-               "Посудомоечная машина",
-               "Мультиварка",
-               "Водонагреватель",
-               "Плита",
-               "Микроволновая печь",
-               "Духовой шкаф",
-               "Холодильник",
-               "Увлажнитель воздуха",
-               "Телевизор",
-               "Пылесос",
-               "Музыкальный центр",
-               "Компьютер",
-               "Игровая консоль"
-           };
+   {
+       "Чайник",
+       "Стиральная машина",
+       "Посудомоечная машина",
+       "Мультиварка",
+       "Водонагреватель",
+       "Плита",
+       "Микроволновая печь",
+       "Духовой шкаф",
+       "Холодильник",
+       "Увлажнитель воздуха",
+       "Телевизор",
+       "Пылесос",
+       "Музыкальный центр",
+       "Компьютер",
+       "Игровая консоль"
+   };
 
-            // Создадим новый стек
             var innerStack = new StackLayout();
 
-            // Сохраним в стек имеющиеся данные, используя свойство Children
             foreach (string deviceName in homeDevices)
             {
-                var deviceLabel = new Label() { Text = $"   {deviceName}", FontSize = 17 };
-                innerStack.Children.Add(new Label());
-                innerStack.Children.Add(deviceLabel);
+                // Создадим текстовый элемент
+                var deviceLabel = new Label() { Text = deviceName, FontSize = 17 };
+
+                // Контейнер Frame, внутри которого будет отображаться текстовый элемент
+                var frame = new Frame()
+                {
+                    BorderColor = Color.Gray,
+                    BackgroundColor = Color.FromHex("#e1e1e1"),
+                    CornerRadius = 4,
+                    Margin = new Thickness(10, 1)
+                };
+
+                // Задаем содержимое контейнера Frame
+                frame.Content = deviceLabel;
+
+                // Добавляем фреймы в стек для их отображения в едином списке по порядку
+                innerStack.Children.Add(frame);
             }
 
             // Сохраним стек внутрь уже имеющегося в xaml-файле блока прокручиваемой разметки
             scrollView.Content = innerStack;
-
         }
     }
 }
