51c9d48684860f08d4606f8cc49d5a8977073b8c
Marat <milya010790@mail.ru>
2024-08-14 19:00:38 +0500

    Костомизация Entry b Editor для дроида

---
+++ HomeApp/HomeApp.Android/Renderers/CustomEditorRenderer.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using HomeApp.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace HomeApp.Droid.Renderers
+{
+    /// <summary>
+    /// Отключаем подчеркивание по умолчанию для элемента Editor на платформе  Android
+    /// </summary>
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
