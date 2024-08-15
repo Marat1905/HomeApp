22d2ed30d6911d538a1769410deadee6234442b6
Marat <milya010790@mail.ru>
2024-08-15 10:41:49 +0500

    Работа с DatePicker

---
+++ HomeApp/HomeApp/Pages/DeviceContolPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeviceContolPage : ContentPage
+    {
+        public DeviceContolPage()
+        {
+            InitializeComponent();
+            GetContent();
+        }
+
+        public void GetContent()
+        {
+            // Создаем виджет выбора даты
+            var datePicker = new DatePicker
+            {
+                Format = "D",
+                // Диапазон дат: +/- неделя
+                MaximumDate = DateTime.Now.AddDays(7),
+                MinimumDate = DateTime.Now.AddDays(-7),
+            };
+
+            var datePickerText = new Label { Text = "Дата запуска ", Margin = new Thickness(0, 20, 0, 0) };
+
+            // Добавляем всё на страницу
+            stackLayout.Children.Add(new Label { Text = "Устройство" });
+            stackLayout.Children.Add(new Entry { BackgroundColor = Color.AliceBlue, Text = "Холодильник" });
+            stackLayout.Children.Add(datePickerText);
+            stackLayout.Children.Add(datePicker);
+            stackLayout.Children.Add(new Button { Text = "Сохранить", BackgroundColor = Color.Silver, Margin = new Thickness(0, 5, 0, 0) });
+
+            // Регистрируем обработчик события выбора даты
+            datePicker.DateSelected += (sender, e) => DateSelectedHandler(sender, e, datePickerText);
+        }
+
+        public void DateSelectedHandler(object sender, DateChangedEventArgs e, Label datePickerText)
+        {
+            // При срабатывании выбора - будет меняться информационное сообщение.
+            datePickerText.Text = "Запустится " + e.NewDate.ToString("dd/MM/yyyy");
+        }
+
+    }
+}
