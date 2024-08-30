f69461c9a1c5023048678905bbceb3a8ec4c1fc0
Marat <milya010790@mail.ru>
2024-08-30 10:15:15 +0500

    охранение конфигурации в Preferences

--- HomeApp/HomeApp/Pages/ProfilePage.xaml.cs
+++ HomeApp/HomeApp/Pages/ProfilePage.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,6 +22,7 @@ namespace HomeApp.Pages
         {
             InitializeComponent();
         }
+
 
         /// <summary>
         /// Вызывается до того, как элемент становится видимым
@@ -42,8 +43,36 @@ namespace HomeApp.Pages
                 App.Current.Properties.Add("CurrentUser", UserInfo);
             }
 
+            // Получим значения ползунков из Preferences.
+            // Если значений нет - установим значения по умолчанию (false)
+            gasSwitch.On = Preferences.Get("gasState", false);
+            climateSwitch.On = Preferences.Get("climateState", false);
+            electroSwitch.On = Preferences.Get("electroState", false);
+
             base.OnAppearing();
         }
+
+        ///// <summary>
+        ///// Вызывается до того, как элемент становится видимым
+        ///// </summary>
+        //protected override void OnAppearing()
+        //{
+        //    // Проверяем, есть ли в словаре значение
+        //    if (App.Current.Properties.TryGetValue("CurrentUser", out object user))
+        //    {
+        //        UserInfo = user as UserInfo;
+        //        loginEntry.Text = UserInfo.Name;
+        //        emailEntry.Text = UserInfo.Email;
+        //    }
+        //    else
+        //    {
+        //        // Добавляем, если нет
+        //        UserInfo = new UserInfo();
+        //        App.Current.Properties.Add("CurrentUser", UserInfo);
+        //    }
+
+        //    base.OnAppearing();
+        //}
 
         private void NotifyUser(object sender, ToggledEventArgs e)
         {
@@ -59,7 +88,24 @@ namespace HomeApp.Pages
             UserInfo.Name = loginEntry.Text;
             UserInfo.Email = emailEntry.Text;
 
+            // Сохраним значения ползунков в настройки.
+            Preferences.Set("gasState", gasSwitch.On);
+            Preferences.Set("climateState", climateSwitch.On);
+            Preferences.Set("electroState", electroSwitch.On);
+
+            // Возврат на предыдущую страницу
             await Navigation.PopAsync();
         }
+
+        ///// <summary>
+        /////  Сохраним информацию о пользователе
+        ///// </summary>
+        //private async void saveButton_Clicked(object sender, System.EventArgs e)
+        //{
+        //    UserInfo.Name = loginEntry.Text;
+        //    UserInfo.Email = emailEntry.Text;
+
+        //    await Navigation.PopAsync();
+        //}
     }
 }
