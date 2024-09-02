47e3141b73db86038568bbdfeb819e9295a7bc97
Marat <milya010790@mail.ru>
2024-09-02 12:08:57 +0500

    класс-расширение для игнорирования ошибки SSL. Для теста пойдет.

---
+++ HomeApp/HomeApp/Extensions/HttpExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+namespace HomeApp.Extensions
+{
+    public static class HttpExtension
+    {
+        /// <summary>
+        /// Отключаем проверку ошибок SSL
+        /// </summary>
+        public static HttpClientHandler GetInsecureHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+            return handler;
+        }
+    }
+}
